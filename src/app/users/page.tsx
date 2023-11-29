d760392013a3ab7c2394c4373f10da37fa151633
Van Chi Hieu <vanchihieu3@gmail.com>
2023-11-29 20:13:58 +0700

    pagination with nextjs

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,14 +1,33 @@
 import UsersTable from "@/components/users/users.table";
 
-const UserPage = async () => {
-  const res = await fetch("http://localhost:8000/users", {
-    method: "GET",
-  });
+const calculatePagesCount = (pageSize: number, totalCount: number) => {
+  return totalCount < pageSize ? 1 : Math.ceil(totalCount / pageSize);
+};
+
+const UserPage = async (props: any) => {
+  const LIMIT = 1;
+  const page = props?.searchParams?.page ?? 1;
+
+  const res = await fetch(
+    `http://localhost:8000/users?_page=${page}&_limit=${LIMIT}`,
+    {
+      method: "GET",
+    }
+  );
+  const total_items = +(res.headers?.get("X-Total-Count") ?? 0);
+  const totalPages = calculatePagesCount(LIMIT, total_items);
   const data = await res.json();
 
   return (
     <div>
-      <UsersTable users={data ? data : []} />
+      <UsersTable
+        users={data ? data : []}
+        meta={{
+          current: +page,
+          pageSize: LIMIT,
+          total: total_items,
+        }}
+      />
     </div>
   );
 };
--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -2,14 +2,24 @@
 import { IUser } from "@/types/backend";
 import { Table } from "antd";
 import { ColumnsType } from "antd/es/table";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 interface IProps {
   users: IUser[] | [];
+  meta: {
+    current: number;
+    pageSize: number;
+    total: number;
+  };
 }
 
 const UsersTable = (props: IProps) => {
-  const { users } = props;
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const { users, meta } = props;
 
   const columns: ColumnsType<IUser> = [
     {
@@ -26,9 +36,33 @@ const UsersTable = (props: IProps) => {
     },
   ];
 
+  const onChange = (pagination: any, filters: any, sorter: any, extra: any) => {
+    if (pagination && pagination.current) {
+      const params = new URLSearchParams(searchParams);
+      params.set("page", pagination.current);
+      replace(`${pathname}?${params.toString()}`);
+    }
+  };
+
   return (
     <div>
-      <Table bordered dataSource={users} columns={columns} />;
+      <Table
+        rowKey={"id"}
+        bordered
+        dataSource={users}
+        columns={columns}
+        onChange={onChange}
+        pagination={{
+          ...meta,
+          showTotal: (total, range) => {
+            return (
+              <div>
+                {range[0]}-{range[1]} trên {total} rows
+              </div>
+            );
+          },
+        }}
+      />
     </div>
   );
 };
